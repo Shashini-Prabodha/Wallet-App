dd0cc64230b07429eeff27c4bd366883ff3fefa7
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2021-07-02 13:26:26 +0530

    project create

---
+++ component/Home.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {BottomNavigation, Text} from 'react-native-paper';
+
+
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const Home = () => {
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        {key: 'music', title: 'Music', icon: 'queue-music'},
+        {key: 'albums', title: 'Albums', icon: 'album'},
+        {key: 'recents', title: 'Recents', icon: 'history'},
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        music: MusicRoute,
+        albums: AlbumsRoute,
+        recents: RecentsRoute,
+    });
+
+    return (
+        <BottomNavigation
+            navigationState={{index, routes}}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+        />
+    );
+};
+
+export default Home;
